8d7aa8e1bc31aebef6c3def35fbd94bc457181da
wenkrang <2934356978@qq.com>
2025-02-22 22:03:15 +0800

    Initial commit

---
+++ src/main/java/com/wenkrang/devTools/Command.java
@@ -0,0 +1,13 @@
+package com.wenkrang.devTools;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Command implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, org.bukkit.command.Command command, String s, String[] strings) {
+
+        return true;
+    }
+}
---
+++ src/main/java/com/wenkrang/devTools/DevTools.java
@@ -0,0 +1,19 @@
+package com.wenkrang.devTools;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DevTools extends JavaPlugin {
+    public static ItemStack itemStack;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
